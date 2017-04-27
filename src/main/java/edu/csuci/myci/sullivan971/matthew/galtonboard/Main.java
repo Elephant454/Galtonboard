511ce0b660172057db7003f317895ea2fd013347
Matthew Sullivan <matthew.sullivan971@myci.csuci.edu>
2017-04-27 13:49:37 -0700

    initial commit

---
+++ src/main/java/edu/csuci/myci/sullivan971/matthew/galtonboard/Main.java
@@ -0,0 +1,48 @@
+package edu.csuci.myci.sullivan971.matthew.galtonboard;
+
+import java.awt.BorderLayout;
+import javax.swing.*;
+
+public class Main {
+
+    public static void main (String[] args) {
+
+        try {
+
+            //System.out.println(UIManager.getSystemLookAndFeelClassName());
+            //UIManager.setLookAndFeel("com.sun.java.swing.plaf.gtk.GTKLookAndFeel");
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        }
+        catch (UnsupportedLookAndFeelException e) {
+            e.printStackTrace();
+           // handle exception
+        }
+        catch (ClassNotFoundException e) {
+            e.printStackTrace();
+           // handle exception
+        }
+        catch (InstantiationException e) {
+            e.printStackTrace();
+           // handle exception
+        }
+        catch (IllegalAccessException e) {
+            e.printStackTrace();
+           // handle exception
+        }
+
+        //Create the window
+        JFrame window = new JFrame("texteditor");
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setLayout(new BorderLayout());
+
+        // Create a panel for the game to run inside
+        GraphicsPanel graphicspanel = new GraphicsPanel();
+        window.add(graphicspanel, BorderLayout.CENTER);
+        //JTextField input = new JTextField();
+        //window.add(input);
+
+        window.pack();
+        window.setSize(800, 600);
+        window.setVisible(true);
+    }
+}
