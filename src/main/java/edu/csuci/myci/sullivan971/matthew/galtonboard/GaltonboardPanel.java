7c7ddd122eca0c183034ba4286f1d9b73b720313
Matthew Sullivan <matthew.sullivan971@myci.csuci.edu>
2017-05-03 12:45:05 -0700

    started to create graphical part

---
+++ src/main/java/edu/csuci/myci/sullivan971/matthew/galtonboard/GaltonboardPanel.java
@@ -0,0 +1,106 @@
+package edu.csuci.myci.sullivan971.matthew.galtonboard;
+
+public class GaltonboardPanel extends JPanel implements KeyListener, Runnable {
+    private int binWidth;
+    private int binHeight;
+
+    private int pegRadius;
+    private int pegDeltaX;
+    private int pegDeltaY;
+
+    private int ballRadius;
+
+    private Galtonboard galtonboard;
+
+    // start of instance variables that I hastily copied over
+    private static final long serialVersionUID = 42L;
+
+    boolean running = true;
+
+    float oldTime = System.nanoTime();
+    float currentTime = oldTime;
+    float deltaTime = 0;
+    float fps = 0;
+
+    final int NANOSECONDS_PER_MILLISECOND = 1000000;
+    final int MILLISECONDS_PER_SECOND = 1000;
+    // there should be 1000 frames every 30 seconds, or 30 frames per second
+    final double TARGET_FRAMERATE = 1.0/30 * MILLISECONDS_PER_SECOND;
+
+    //RandomPalette randomPalette = new RandomPalette(0.5f, 0.6f);
+    RandomPalette randomPalette = new RandomPalette(0.8f, 0.7f);
+
+    ArrayDeque<Character> charQueue = new ArrayDeque<>();
+    ArrayDeque<Color> colorQueue = new ArrayDeque<>();
+
+    JLabel label = new JLabel();
+    String labelText = "";
+
+    int fontSize = 12;
+
+    Thread drawingThread = new Thread(this);
+
+    public GaltonboardPanel(Galtonboard galtonboard) {
+        this.galtonboard = galtonboard;
+
+
+        // start of constructor that I hastily copied over
+        this.setLayout(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.weightx = 1;
+        gbc.weighty = 1;
+        gbc.anchor = GridBagConstraints.NORTHWEST;
+        this.add(label, gbc);
+
+        colorQueue.add(Color.BLACK);
+
+        setFocusable(true);
+        addKeyListener(this);
+        this.setSize(800, 600);
+        this.requestFocusInWindow(true);
+        drawingThread.start();
+    }
+
+    
+    @Override
+    public void paintComponent (Graphics g) {
+        Graphics2D g2 = (Graphics2D) g;
+
+        g2.clearRect(0, 0, getWidth(), getHeight());
+
+        g2.setFont(new Font("Consolas", Font.PLAIN, fontSize));
+    }
+
+    long sleepLength;
+    public void run() {
+        while(running) {
+            currentTime = System.nanoTime();
+
+            // do the actual logic for this tick
+            parseInput();
+            repaint();
+
+            // use the difference in time to determine how long we should sleep
+            // for this tick
+            deltaTime = currentTime-oldTime;
+            oldTime = currentTime;
+            fps = 1/deltaTime;
+            sleepLength = (long) TARGET_FRAMERATE
+                    - (long) (deltaTime/NANOSECONDS_PER_MILLISECOND);
+            try {
+                if(sleepLength > -1) drawingThread.sleep(sleepLength);
+                else drawingThread.sleep(0);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+    public void keyTyped(KeyEvent e){
+        charQueue.push(e.getKeyChar());
+        System.out.println(labelText);
+    }
+    public void keyPressed(KeyEvent e){}
+    public void keyReleased(KeyEvent e){}
+}
